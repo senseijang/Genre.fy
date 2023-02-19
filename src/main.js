b386c727a5a09902f62d6204588aeec6e0c7c11f
boooiil <boooiil@outlook.com>
2023-02-19 13:20:07 -0500

    rm compiled js in src

--- src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var spotifyOauth_1 = __importDefault(require("./spotify/spotifyOauth"));
-var app = new spotifyOauth_1["default"]();
-app.startExpress();
