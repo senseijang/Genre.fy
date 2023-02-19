50a8d5b804f3dfa86875c51fcfa1cacb9094bb9f
boooiil <boooiil@outlook.com>
2023-02-19 18:34:21 -0500

    finish user class

--- src/classes/user.ts
+++ src/classes/user.ts
@@ -1,3 +1,5 @@
+import request from "request";
+import { log } from "../logging/logger";
 import { Artist } from "./artist";
 
 export class User {
@@ -6,5 +8,53 @@ export class User {
     private token: string;
     private topArtist: Artist;
     private expires: number;
+
+    constructor(token: string) {
+
+        this.token = token;
+        this.expires = Date.now() + 3600000
+
+    }
+
+    fetch() {
+
+        request.get('https://api.spotify.com/v1/me', {
+
+            headers: {
+
+                "Authorization": `Bearer ${this.token}`,
+                "Content-Type": "application/json"
+
+            }
+
+        }, (err, res, body) => {
+
+            if (err) return log(err)
+
+            this.name = body.display_name;
+
+        })
+
+        request.get('https://api.spotify.com/v1/me/top/artists?limit=1', {
+
+            headers: {
+
+                "Authorization": `Bearer ${this.token}`,
+                "Content-Type": "application/json"
+
+            }
+
+        }, (err, res, body) => {
+
+            if (err) throw log(err)
+
+            body = JSON.parse(body).items[0];
+
+            this.topArtist = new Artist(body.name, body.id, body.external_urls.spotify, body.images[0].url, this.token)
+            this.topArtist.fetch()
+
+        })
+
+    }
 
 }
