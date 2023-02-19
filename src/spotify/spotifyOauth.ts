dd792c050b13f0772eb99ac0a15132989a6fd87a
boooiil <boooiil@outlook.com>
2023-02-19 18:33:40 -0500

    finish getting user token

--- src/spotify/spotifyOauth.ts
+++ src/spotify/spotifyOauth.ts
@@ -5,15 +5,18 @@
 
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {Application, Request, Response} from "express";
+import { Application, Request, Response } from "express";
 import express from "express";
 import request from "request";
 import spotifyConfig from "../config/spotify.json";
+import { log } from "../logging/logger";
+import { InternalState } from "../objects/state";
+import { User } from "../classes/user";
 
 class SpotifyOauth {
     public app!: Application;
 
-    public USER_TOKEN: string;
+    private AUTH_CODE: string;
 
     private readonly CLIENT_ID = spotifyConfig.ID;
     private readonly CLIENT_SECRET = spotifyConfig.SECRET;
@@ -31,13 +34,42 @@ class SpotifyOauth {
         app.get("/callback", (req, res) => this.getCallback(req, res));
         app.get("/refresh_token", (req, res) => this.getRefreshToken(req, res));
 
-        // tslint:disable-next-line:no-console
-        console.log("Listening on 8000");
+        log(`green Listening on: http://localhost:8000`)
         app.listen(8000);
 
         app.get("/success", (req, response) => {
 
+            if (!response.req.query.code) return response.send("401 - Not authorized.")
+
             response.sendFile("./static/success.html", { root: "." });
+
+            this.AUTH_CODE = response.req.query.code.toString();
+
+            request.post(`https://accounts.spotify.com/api/token`, {
+
+                form: {
+                    code: this.AUTH_CODE,
+                    redirect_uri: this.REDIRECT_URI,
+                    grant_type: "authorization_code"
+                },
+                headers: {
+
+                    "Authorization": "Basic " + (Buffer.from(this.CLIENT_ID + ":" + this.CLIENT_SECRET)
+                        .toString("base64"))
+                },
+                json: true
+
+            }, (err, res, body) => {
+
+                if (body.access_token) {
+
+                    (InternalState[this.STATE_KEY] = new User(body.access_token)).fetch();
+
+                }
+
+                else response.send("500 - Internal error")
+
+            })
 
         });
     }
@@ -72,7 +104,7 @@ class SpotifyOauth {
                     redirect_uri: this.REDIRECT_URI,
                 },
                 headers: {
-                    Authorization: "Basic " + (new Buffer(this.CLIENT_ID + ":" + this.CLIENT_SECRET)
+                    Authorization: "Basic " + (Buffer.from(this.CLIENT_ID + ":" + this.CLIENT_SECRET)
                         .toString("base64")),
                 },
                 json: true,
@@ -80,13 +112,14 @@ class SpotifyOauth {
             };
 
             request.post(authOptions, (error, response, body) => {
+                log("User token:", body.access_token)
                 if (!error && response.statusCode === 200) {
-
-                    this.USER_TOKEN = body.access_token;
+                    const USER_TOKEN = body.access_token;
                     const refresh_token = body.refresh_token;
+                    log("User token:", USER_TOKEN, "refresh token", refresh_token)
 
                     const options = {
-                        headers: {Authorization: "Bearer " + this.USER_TOKEN},
+                        headers: { Authorization: "Bearer " + USER_TOKEN },
                         json: true,
                         url: "https://api.spotify.com/v1/me",
                     };
@@ -98,7 +131,7 @@ class SpotifyOauth {
                     });
 
                     // we can also pass the token to the browser to make requests from there
-                    res.redirect("/#" + `access_token=${this.USER_TOKEN}&refresh_token=${refresh_token}`);
+                    res.redirect("/#" + `access_token=${USER_TOKEN}&refresh_token=${refresh_token}`);
                 } else {
                     res.redirect("/#" + `error=invalid_token`);
                 }
@@ -114,17 +147,23 @@ class SpotifyOauth {
                 grant_type: "refresh_token",
                 refresh_token,
             },
-            headers: {Authorization: "Basic " + (new Buffer(this.CLIENT_ID + ":" + this.CLIENT_SECRET)
-                .toString("base64"))},
+            headers: {
+                Authorization: "Basic " + (Buffer.from(this.CLIENT_ID + ":" + this.CLIENT_SECRET)
+                    .toString("base64"))
+            },
             json: true,
             url: "https://accounts.spotify.com/api/token",
         };
 
         request.post(authOptions, (error, response, body) => {
+            log("User token:", body.access_token)
             if (!error && response.statusCode === 200) {
-                const access_token = body.access_token;
+
+                const USER_TOKEN = body.access_token;
+                log("User token:", USER_TOKEN)
+
                 res.send({
-                    access_token,
+                    access_token: USER_TOKEN,
                 });
             }
         });
@@ -138,6 +177,7 @@ class SpotifyOauth {
             text += possible.charAt(Math.floor(Math.random() * possible.length));
         }
         return text;
-    }}
+    }
+}
 
 export default SpotifyOauth;
